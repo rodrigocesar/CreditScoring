ad019ab8196f381f170d938733222939ccdbf92e
rodrigo <rodrigo.cesar.silva@accenture.com>
2019-01-25 12:24:31 -0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='As a credit company, it is important to know beforehand who is able to pay their loans and who is not.',
+    author='Rodrigo',
+    license='',
+)
